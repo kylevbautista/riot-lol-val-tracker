f34066417d350a2ddeea72090a3bde71e75a7311
Kyle Bautista <kylebautista@Kyles-M1-MacBook-Pro.local>
2021-11-07 16:44:52 -0700

    fix redux store for production

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,5 @@
 if (process.env.NODE_ENV === "production") {
+  console.log("production store");
   module.exports = require("./configureStore.prod");
 } else {
   module.exports = require("./configureStore.dev");
--- src/redux/configureStore.prod.js
+++ src/redux/configureStore.prod.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 // since rootReducer's file is index.js no need to specify file, implied
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
@@ -6,12 +6,5 @@ import thunk from "redux-thunk";
 // initialState is so we can initialize our store with some
 // data when we create it
 export default function configureStore(initialState) {
-  // add support for redux dev tools
-  const composeEhancers = compose;
-
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEhancers(applyMiddleware(thunk))
-  );
+  return createStore(rootReducer, initialState, applyMiddleware(thunk));
 }
