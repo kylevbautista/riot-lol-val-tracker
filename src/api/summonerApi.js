8394e7fccd6b96257ff5cec0f21a996ed9b0dfd4
Kyle Bautista <kylebautista@Kyles-M1-MacBook-Pro.local>
2021-11-08 06:42:29 -0700

    update api key

--- src/api/summonerApi.js
+++ src/api/summonerApi.js
@@ -1,7 +1,7 @@
 import { handleResponse, handleError } from "./apiUtils";
 const baseUrl =
   "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
-const apiKey = "RGAPI-e0c88b9c-8172-4bd0-915c-664741f89cd0";
+const apiKey = "RGAPI-4aecef72-d888-47cc-8a43-804488a203cb";
 //const apiKey = process.env.REACT_APP_RIOT_API;
 
 // Get summoner data given a name
