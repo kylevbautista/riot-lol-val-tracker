b98be9d5bfea124f5310132735a48d02aa399e34
Kyle Bautista <kylebautista@Kyles-M1-MacBook-Pro.local>
2021-11-11 17:26:59 -0800

    modify api calls to improve readablility

--- src/api/summonerApi.js
+++ src/api/summonerApi.js
@@ -5,37 +5,66 @@ const apiKey = "RGAPI-4fa39b17-6571-430f-9056-7f25b5543eeb";
 //const apiKey = process.env.REACT_APP_RIOT_API;
 
 // Get summoner data given a name
-export function byName(name) {
-  // to avoid es-lint error using template literal instead of classic string concatenation
-  let url = `${baseUrl}${name}?api_key=${apiKey}`;
-  return fetch(url).then(handleResponse).catch(handleError);
-}
+export const byName = async (name) => {
+  try {
+    let url = `${baseUrl}${name}?api_key=${apiKey}`;
+    const response = await fetch(url);
+    const data = await handleResponse(response);
+    return data;
+  } catch (err) {
+    handleError(err);
+  }
+};
 
 // Get 20 most recent match id's for a summoner given a summoner's puuid
-export function getMatchIds(puuid) {
-  let baseUrl =
-    "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/";
-  let url = `${baseUrl}${puuid}/ids?start=0&count=20&api_key=${apiKey}`;
-  return fetch(url).then(handleResponse).catch(handleError);
-}
+export const getMatchIds = async (puuid) => {
+  try {
+    let baseUrl =
+      "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/";
+    let url = `${baseUrl}${puuid}/ids?start=0&count=20&api_key=${apiKey}`;
+    const response = await fetch(url);
+    const data = await handleResponse(response);
+    return data;
+  } catch (err) {
+    handleError(err);
+  }
+};
 
 // Get the match details for a given match id
-export function getMatchData(matchId) {
-  let baseUrl = "https://americas.api.riotgames.com/lol/match/v5/matches/";
-  let url = `${baseUrl}${matchId}?api_key=${apiKey}`;
-  return fetch(url).then(handleResponse).catch(handleError);
-}
+export const getMatchData = async (matchId) => {
+  try {
+    let baseUrl = "https://americas.api.riotgames.com/lol/match/v5/matches/";
+    let url = `${baseUrl}${matchId}?api_key=${apiKey}`;
+    const response = await fetch(url);
+    const data = await handleResponse(response);
+    return data;
+  } catch (err) {
+    handleError(err);
+  }
+};
 
-export function getRank(encryptedId) {
-  let baseUrl =
-    "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/";
-  let url = `${baseUrl}${encryptedId}?api_key=${apiKey}`;
-  return fetch(url).then(handleResponse).catch(handleError);
-}
+export const getRank = async (encryptedId) => {
+  try {
+    let baseUrl =
+      "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/";
+    let url = `${baseUrl}${encryptedId}?api_key=${apiKey}`;
+    const response = fetch(url);
+    const data = await handleResponse(response);
+    return data;
+  } catch (err) {
+    handleError(err);
+  }
+};
 
-export function getLeaderBoards() {
-  let baseUrl =
-    "https://na1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5";
-  let url = `${baseUrl}?api_key=${apiKey}`;
-  return fetch(url).then(handleResponse).catch(handleError);
-}
+export const getLeaderBoards = async () => {
+  try {
+    let baseUrl =
+      "https://na1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5";
+    let url = `${baseUrl}?api_key=${apiKey}`;
+    const response = await fetch(url);
+    const data = await handleResponse(response);
+    return data;
+  } catch (err) {
+    handleError(err);
+  }
+};
--- src/api/valorantApi.js
+++ src/api/valorantApi.js
@@ -2,14 +2,26 @@ import { handleResponse, handleError } from "./apiUtils";
 const apiKey = "RGAPI-4fa39b17-6571-430f-9056-7f25b5543eeb";
 //const apiKey = process.env.REACT_APP_RIOT_API;
 
-export function getContent() {
-  let baseUrl = "https://na.api.riotgames.com/val/content/v1/contents";
-  let url = `${baseUrl}?api_key=${apiKey}`;
-  return fetch(url).then(handleResponse).catch(handleError);
-}
-export function getLeaderBoards(actid) {
-  let baseUrl =
-    "https://na.api.riotgames.com/val/ranked/v1/leaderboards/by-act/";
-  let url = `${baseUrl}${actid}?size=200&startIndex=0&api_key=${apiKey}`;
-  return fetch(url).then(handleResponse).catch(handleError);
-}
+export const getContent = async () => {
+  try {
+    let baseUrl = "https://na.api.riotgames.com/val/content/v1/contents";
+    let url = `${baseUrl}?api_key=${apiKey}`;
+    const response = await fetch(url);
+    const data = await handleResponse(response);
+    return data;
+  } catch (err) {
+    handleError(err);
+  }
+};
+export const getLeaderBoards = async (actid) => {
+  try {
+    let baseUrl =
+      "https://na.api.riotgames.com/val/ranked/v1/leaderboards/by-act/";
+    let url = `${baseUrl}${actid}?size=200&startIndex=0&api_key=${apiKey}`;
+    const response = await fetch(url);
+    const data = await handleResponse(response);
+    return data;
+  } catch (err) {
+    handleError(err);
+  }
+};
