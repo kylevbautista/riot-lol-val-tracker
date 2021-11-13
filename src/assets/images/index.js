c4e00cf2585aa39411101b8b7a7a91cc6d8dedf4
Kyle Bautista <kylebautista@Kyles-M1-MacBook-Pro.local>
2021-11-12 18:26:29 -0800

    add more exported assets

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -36,6 +36,47 @@ import Gragas from "./champion-squares-compressed/Gragas_Square_0-compressor.png
 import Graves from "./champion-squares-compressed/Graves_Square_0-compressor.png";
 import Hecarim from "./champion-squares-compressed/Hecarim_Square_0-compressor.png";
 import Heimerdinger from "./champion-squares-compressed/Heimerdinger_Square_0-compressor.png";
+import Illaoi from "./champion-squares-compressed/Illaoi_Square_0-compressor.png";
+import Irelia from "./champion-squares-compressed/Irelia_Square_0-compressor.png";
+import Ivern from "./champion-squares-compressed/Ivern_Square_0-compressor.png";
+import Janna from "./champion-squares-compressed/Janna_Square_0-compressor.png";
+import Jax from "./champion-squares-compressed/Jax_Square_0-compressor.png";
+import Jayce from "./champion-squares-compressed/Jayce_Square_0-compressor.png";
+import Jhin from "./champion-squares-compressed/Jhin_Square_0-compressor.png";
+import Jinx from "./champion-squares-compressed/Jinx_Square_0-compressor.png";
+import Kalista from "./champion-squares-compressed/Kalista_Square_0-compressor.png";
+import Karma from "./champion-squares-compressed/Karma_Square_0-compressor.png";
+import Karthus from "./champion-squares-compressed/Karthus_Square_0-compressor.png";
+import Kassadin from "./champion-squares-compressed/Kassadin_Square_0-compressor.png";
+import Katarina from "./champion-squares-compressed/Katarina_Square_0-compressor.png";
+import Kayle from "./champion-squares-compressed/Kayle_Square_0-compressor.png";
+import Kennen from "./champion-squares-compressed/Kennen_Square_0-compressor.png";
+import Khazix from "./champion-squares-compressed/Khazix_Square_0-compressor.png";
+import Kindred from "./champion-squares-compressed/Kindred_Square_0-compressor.png";
+import Kled from "./champion-squares-compressed/Kled_Square_0-compressor.png";
+import KogMaw from "./champion-squares-compressed/KogMaw_Square_0-compressor.png";
+import Leblanc from "./champion-squares-compressed/Leblanc_Square_0-compressor.png";
+import LeeSin from "./champion-squares-compressed/LeeSin_Square_0-compressor.png";
+import Leona from "./champion-squares-compressed/Leona_Square_0-compressor.png";
+import Lissandra from "./champion-squares-compressed/Lissandra_Square_0-compressor.png";
+import Lucian from "./champion-squares-compressed/Lucian_Square_0-compressor.png";
+import Lulu from "./champion-squares-compressed/Lulu_Square_0-compressor.png";
+import Lux from "./champion-squares-compressed/Lux_Square_0-compressor.png";
+import Malphite from "./champion-squares-compressed/Malphite_Square_0-compressor.png";
+import Malzahar from "./champion-squares-compressed/Malzahar_Square_0-compressor.png";
+import Maokai from "./champion-squares-compressed/Maokai_Square_0-compressor.png";
+import MasterYi from "./champion-squares-compressed/MasterYi_Square_0-compressor.png";
+import MissFortune from "./champion-squares-compressed/MissFortune_Square_0-compressor.png";
+import MonkeyKing from "./champion-squares-compressed/MonkeyKing_Square_0-compressor.png";
+import Mordekaiser from "./champion-squares-compressed/Mordekaiser_Square_0-compressor.png";
+import Morgana from "./champion-squares-compressed/Morgana_Square_0-compressor.png";
+import Nami from "./champion-squares-compressed/Nami_Square_0-compressor.png";
+import Nasus from "./champion-squares-compressed/Nasus_Square_0-compressor.png";
+import Nidalee from "./champion-squares-compressed/Nidalee_Square_0-compressor.png";
+import Nocturne from "./champion-squares-compressed/Nocturne_Square_0-compressor.png";
+import Nunu from "./champion-squares-compressed/Nunu_Square_0-compressor.png";
+import Olaf from "./champion-squares-compressed/Olaf_Square_0-compressor.png";
+import Orianna from "./champion-squares-compressed/Orianna_Square_0-compressor.png";
 
 export {
   Aatrox,
@@ -76,4 +117,45 @@ export {
   Graves,
   Hecarim,
   Heimerdinger,
+  Illaoi,
+  Irelia,
+  Ivern,
+  Janna,
+  Jax,
+  Jayce,
+  Jhin,
+  Jinx,
+  Kalista,
+  Karma,
+  Karthus,
+  Kassadin,
+  Katarina,
+  Kayle,
+  Kennen,
+  Khazix,
+  Kindred,
+  Kled,
+  KogMaw,
+  Leblanc,
+  LeeSin,
+  Leona,
+  Lissandra,
+  Lucian,
+  Lulu,
+  Lux,
+  Malphite,
+  Malzahar,
+  Maokai,
+  MasterYi,
+  MissFortune,
+  MonkeyKing,
+  Mordekaiser,
+  Morgana,
+  Nami,
+  Nasus,
+  Nidalee,
+  Nocturne,
+  Nunu,
+  Olaf,
+  Orianna,
 };
